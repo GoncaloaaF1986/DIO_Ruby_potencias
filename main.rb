596b4f5843bb353614fe987779dc976193fac305
Goncalo Feliciano <goncaloaaf@gmail.com>
2025-01-19 14:22:50 +0000

    Criação do file

--- main.rb
+++ main.rb
@@ -1,2 +1,11 @@
-# frozen_string_literal: true
+n = []
 
+3.times do |count|
+  print "Digite o num #{count + 1}: "
+  n.push(gets.chomp.to_i)
+end
+
+
+n.each do |i|
+  puts "#{i} ao cubo = #{i**3}"
+end
